47ca131e94e88f45aadd8945f9d24f1da92d7998
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-14 18:24:48 +0300

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removables) {
+    for(let i = 0; i < removables.length; i++) {
+        while(array.indexOf(removables[i]) !== -1) {
+            array.splice(array.indexOf(removables[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

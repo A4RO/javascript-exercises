3b5c01be62c82f6d17f0eef5394c348b6f7a2c41
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-14 23:31:50 +0300

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+  let sum = 0;
+	for(let i = 0; i < numbers[0].length; i++) {
+    sum += numbers[0][i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  let multiplied = 1;
+	for(let i = 0; i < numbers[0].length; i++) {
+    multiplied *= numbers[0][i];
+  }
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorial = 1;
+  for(let i = 1; i <= number; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line

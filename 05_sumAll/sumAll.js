e7b843b1e10309dac8c69d687a3891b7430d8dc7
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-14 18:42:26 +0300

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0 || Number.isInteger(start) === false || Number.isInteger(end) === false) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+
+    if(start < end) {
+        for(; start <= end; start++) {
+            sum += start
+        }
+    }
+    else {
+        for(; start >= end; start--) {
+            sum += start
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line

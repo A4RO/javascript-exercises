a7d8396e16c8472c242b26112b422e2f7d2d6f2b
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-14 18:03:25 +0300

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = [];
+    arr = [...string];
+    let reversedString = "";
+    for(let i = 0; i < arr.length; i++) {
+        reversedString += arr[arr.length-1-i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line

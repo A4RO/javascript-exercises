0f09bd499149915b0705bdae98fb8a18b157c431
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-28 15:10:12 +0300

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let punctuation = /[\.,?!\s]/g;
+    strWithoutPunctuation =  str.replace(punctuation, "");
+    return str.split('').reverse().join('').toLowerCase().replace(punctuation, "").includes(strWithoutPunctuation.toLowerCase());
 };
 
 // Do not edit below this line

3b5e48001faa56079a8f017d5599c0b5f3506079
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-28 16:26:06 +0300

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        if(a.yearOfDeath === undefined) {
+        let currentYear = new Date().getFullYear();
+        return (currentYear - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);  
+        }
+        else if(b.yearOfDeath === undefined) {
+            let currentYear = new Date().getFullYear();
+            return (a.yearOfDeath - a.yearOfBirth) - (currentYear - b.yearOfBirth);
+        }
+        return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+    });
+    return arr[arr.length-1];
 };
 
 // Do not edit below this line

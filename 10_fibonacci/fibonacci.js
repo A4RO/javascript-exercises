7387a4a0df7fdebc5a282592739c853ed9b44c1c
Aaro Heikkinen <heikkinenaaro@gmail.com>
2024-09-28 15:27:33 +0300

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number == 0) return 0;
+    if(number < 0) return "OOPS";
+    let sequence = [];
+    for(let i = 0; i < number; i++) {
+        if(i === 0 || i === 1) {
+            sequence.push(1);
+        }
+        else {
+            sequence.push(sequence[i-2] + sequence[i-1]);
+        }
+    }
+    return sequence[sequence.length-1];
 };
 
 // Do not edit below this line
